9eef6d48107040b523abb02fdf9003edc151a144
vin0414 <vinmogate@gmail.com>
2025-04-16 18:15:52 +0800

    add job creation page

--- app/Views/main/job/create-job.php
+++ app/Views/main/job/create-job.php
@@ -75,11 +75,98 @@
             <!-- BEGIN PAGE BODY -->
             <div class="page-body">
                 <div class="container-xl">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="card-title"><i class="ti ti-briefcase"></i>&nbsp;Create</div>
+                    <form method="POST" class="row g-3" id="frmCreate">
+                        <?=csrf_field()?>
+                        <div class="col-lg-8">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-title"><i class="ti ti-briefcase"></i>&nbsp;Job Details</div>
+                                    <div class="row g-3">
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-4">
+                                                    <div class="form-label">Category</div>
+                                                    <select name="category" class="form-select" required>
+                                                        <option value="">Choose</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="form-label">Assignment</div>
+                                                    <select name="assignment" class="form-select" required>
+                                                        <option value="">Choose</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="form-label">Employment Status</div>
+                                                    <select name="employment" class="form-select" required>
+                                                        <option value="">Choose</option>
+                                                        <option>Full-Time</option>
+                                                        <option>Part-Time</option>
+                                                        <option>Project-Based</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-3">
+                                                    <div class="form-label">Item No</div>
+                                                    <input type="text" name="item_number" class="form-control" required>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="form-label">Position Code</div>
+                                                    <input type="text" name="code" class="form-control" required>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="form-label">Job Level</div>
+                                                    <select name="job_level" class="form-select" required>
+                                                        <option value="">Choose</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <div class="form-label">Salary Grade</div>
+                                                    <select name="salary_grade" class="form-select" required>
+                                                        <option value="">Choose</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="form-label">Position</div>
+                                            <input type="text" class="form-control" name="position" required/>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="form-label">Job Description</div>
+                                            <textarea name="description" class="form-control" style="height: 200px;" required></textarea>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-4">
+                                                    <div class="form-label">Salary Rates</div>
+                                                    <input type="text" name="rates" class="form-control" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="form-label">Posting Date</div>
+                                                    <input type="date" name="posting_date" class="form-control" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="form-label">Closing Date</div>
+                                                    <input type="date" name="closing_date" class="form-control" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        <div class="col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-title"><i class="ti ti-target-arrow"></i>&nbsp;Qualifications</div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
             <!-- END PAGE BODY -->
